8e269206c09b4e3476f954f40c0b08b6a7fd4397
Alan Hardman <alanaktion@gmail.com>
2019-10-21 10:07:41 -0600

    Adding Whomp ripper
    Closes #1

---
+++ whomp.php
@@ -0,0 +1,53 @@
+<?php
+// Whomp
+
+// Another Hiveworks comic, pretty straightforward.
+
+$html = file_get_contents('http://www.whompcomic.com/');
+preg_match('@/comics/(.+\\.(jpg|png|gif))@', $html, $matches);
+
+if (empty($matches[1])) {
+    echo "No comic found on home page! :(\n";
+    return;
+}
+
+if (!is_dir('whomp')) {
+    mkdir('whomp');
+}
+
+// Download current page's comic, load previous comic webpage, repeat
+while (true) {
+    if (is_file('whomp/' . $matches[1])) {
+        return;
+    }
+
+    echo "Downloading {$matches[1]}\n";
+    $url = "http://www.whompcomic.com/comics/{$matches[1]}";
+    $data = @file_get_contents($url);
+    if ($data) {
+        file_put_contents("whomp/{$matches[1]}", $data);
+    }
+
+    // Find previous page link
+    $regex = '@rel="prev" href="(http://www.whompcomic.com/comic/[0-9a-zA-Z-]+)"@';
+    preg_match($regex, $html, $prevMatch);
+
+    if (empty($prevMatch[1])) {
+        echo "No previous URL found!\n";
+        return;
+    }
+
+    $html = @file_get_contents($prevMatch[1]);
+    if (!$html) {
+        echo "Failed to load previous page!\n";
+        return;
+    }
+
+    preg_match('@/comics/(.+\\.(jpg|png|gif))@', $html, $matches);
+    if (empty($matches[1])) {
+        echo "No image found on page!\n";
+        return;
+    }
+
+    usleep(5e5);
+}
